167b6d5b934e2cfb46bd0e88c3efb4a9092e29f7
Nyxouu <justineperso03@gmail.com>
2025-03-18 21:37:02 +0100

    ajustement text page home

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -6,10 +6,19 @@ export default function Home() {
   return (
     <div class="home-content">
       <h1>Bienvenue, voyageurs, dans le monde fascinant d’Orion ! </h1>
-      <p>Que vous soyez un aventurier en quête de merveilles ou un explorateur curieux des mystères d’ailleurs, vous êtes ici chez vous. Orion vous ouvre ses portes, et plus particulièrement celles des continents d'Altair et de Garakh, terres aux mille récits et aux créatures aussi enchanteresses que redoutables. <br></br> <br></br>
-      Ce site est votre guide à travers ces vastes contrées. Vous y trouverez toutes les informations nécessaires pour mieux comprendre cet univers, ses peuples, ses légendes et ses dangers. Des forêts profondes d’Altair aux montagnes obscures de Garakh, chaque recoin recèle des secrets à découvrir, des communautés aux coutumes fascinantes et des créatures aux intentions parfois bienveillantes, parfois malveillantes. <br></br> <br></br><br></br>
-      Alors, préparez-vous… L’aventure commence ici !</p>
+
+      <p>Au-delà des cieux étoilés, bien loin des mondes connus, s’étend Orion, un univers aussi ancien que mystérieux. Ici, le vent murmure des légendes oubliées, et chaque pierre porte l’empreinte de civilisations disparues. Vous qui avez osé franchir le seuil de cet espace sacré, sachez que votre périple ne fait que commencer.</p>
+
+      <p>Les terres d’Altair et de Garakh, les deux continents centraux d’Orion, recèlent des secrets que seuls les plus intrépides parviennent à percer. D’un côté, Altair, un royaume de lumière et de savoir, où les cités suspendues aux falaises tutoient les nuages et où les vastes plaines d’herbes argentées dissimulent des ruines millénaires. Ses habitants, sages et érudits, transmettent leur savoir à travers d’antiques manuscrits, mais gare aux illusions trompeuses : chaque vérité ici cache une énigme plus profonde. </p>
+
+      <p>Mais Orion ne se limite pas à ces deux continents. Bien d’autres territoires, inexplorés ou perdus, attendent ceux qui osent braver l’inconnu. Des archipels flottants d’Isméria, où le ciel et l’eau ne font qu’un, aux déserts hantés d’Aza’kar, où les mirages peuvent devenir réels… Chaque pas est un pari, chaque décision peut forger une destinée. </p>
+
+      <p>Ce site est votre carte, votre boussole, votre guide dans cette épopée. Vous y découvrirez les récits des peuples qui peuplent Orion, leurs traditions, leurs conflits et leurs légendes. Vous plongerez dans l’histoire de ce monde, déchiffrerez ses prophéties et percerez les mystères de ses créatures, qu’elles soient enchanteresses ou redoutables.</p>
+
+      <p>Alors, préparez-vous… L’aventure commence ici !</p>
     </div>
   )
 
 }
+
+
